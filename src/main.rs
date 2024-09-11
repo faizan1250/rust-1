12a693a6e49782e323f5bf20bd905265194a4233
faizan1250 <mdf998865@gmail.com>
2024-09-12 02:57:09 +0530

    "fibonacci in rust"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    println!("{}",is_even(31));
+    println!("{}",is_even(7));
 }
 
-fn is_even(num: i32) -> bool {
-    if num% 2 ==0 {
-        return true;
+fn is_even(num: i32) -> i32 {
+    if num ==0 || num ==1 {
+        return num;
     }
-    return false;
+    return is_even(num-1) + is_even(num-2);
 }
